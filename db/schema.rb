5fb048b1a8c47b0b0638e5abffb6502b9a736343
basil <basil.mawejje@gmail.com>
2017-10-06 10:58:04 +0200

    Change model name from category to restaurant_category

--- db/schema.rb
+++ db/schema.rb
@@ -33,4 +33,14 @@ ActiveRecord::Schema.define(version: 20171006083347) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
+  end
+
+  add_foreign_key "restaurants", "restaurant_categories", column: "category_id"
 end
